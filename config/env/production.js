1429d37b4dee1857ef13b607469972fe33735c24
Jake-Smullin <js.1992@live.com>
2015-09-01 04:07:35 -0700

    adding facebook social login

--- config/env/production.js
+++ config/env/production.js
@@ -29,8 +29,8 @@ module.exports = {
 		callbackURL: '/auth/facebook/callback'
 	},
 	twitter: {
-		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		clientID: process.env.TWITTER_KEY || 'PP9wyVx9hmZgE7aLbYb4XfoYS',
+		clientSecret: process.env.TWITTER_SECRET || 'TITb95VtTXztXXW2ps2AyRl1Cw7JHWmFqHEmVtOZLz4tPOZNRW',
 		callbackURL: '/auth/twitter/callback'
 	},
 	google: {
